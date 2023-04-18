fe6d7ee09758b5d7883b2af63f4df387d323abd6
liugaowei <liugaowei.sd@chinatelecom.cn>
2023-04-18 17:29:29 +0800

    加群链接

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,4 +4,9 @@ export default navbar([
   "/",
   "/home",
   { text: "使用指南", icon: "creative", link: "/guide/" },
+  {
+    text: "加入Q群(752801820)",
+    icon: "group",
+    link: "https://jq.qq.com/?_wv=1027&k=bMNZfGxs",
+  },
 ]);
