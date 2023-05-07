ae03a886b237fab63f04774ec8dfa456b3a9fa03
liugaowei <liugaowei.sd@chinatelecom.cn>
2023-05-07 12:18:23 +0800

    反馈2群

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,8 +5,19 @@ export default navbar([
   "/home",
   { text: "使用指南", icon: "creative", link: "/guide/" },
   {
-    text: "加入Q群(752801820)",
-    icon: "proxy",
-    link: "https://jq.qq.com/?_wv=1027&k=bMNZfGxs",
+    text: "加入Q群",
+    icon: "group",
+    children: [
+      {
+        text: "tts-vue问题反馈群②(702034846)",
+        icon: "people",
+        link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=5-vK4VI8ueWoF-cBDl6PjBzfDeWRqmvw&authKey=ZHIxqKeYEGewEZFtKtT5o1UNbTtAEnMOe4vH2haBIxA8edc1U%2F4rn5R9RKSv5%2BRF&noverify=0&group_code=702034846",
+      },
+      {
+        text: "tts-vue问题反馈群①(752801820)-已满",
+        icon: "group",
+        link: "https://jq.qq.com/?_wv=1027&k=bMNZfGxs",
+      },
+    ],
   },
 ]);
