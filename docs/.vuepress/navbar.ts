beb6e6558bf0766b2b105ff32c76f73f812177ee
liugaowei <liugaowei.sd@chinatelecom.cn>
2023-07-20 08:56:07 +0800

    更新加群链接

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -9,17 +9,27 @@ export default navbar([
     icon: "group",
     children: [
       {
-        text: "tts-vue问题反馈群③(474128303)",
+        text: "tts-vue反馈群⑤(439382846)",
+        icon: "group",
+        link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=aFtHnpeEerVM7UCV8NTSeqsQrUPgo0G2&authKey=g1KT%2F7xaoa%2FT3usmwk5YRIIdkzeJ4bW6tsWCgcopbzCRTwseXTGQS54ZDTmRPmt6&noverify=0&group_code=439382846",
+      },
+      {
+        text: "tts-vue反馈群④(781659118)-已满",
+        icon: "group",
+        link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=AtvxF5GXuLzd8Y9lhPcEnsOddAUO_2Db&authKey=amRtcf3%2FiYqERTYl7gPxXnaPCaKc8CXmewlQKnWoSdxjk07xuLMG1JKvRI7mLysG&noverify=0&group_code=781659118",
+      },
+      {
+        text: "tts-vue反馈群③(474128303)-已满",
         icon: "group",
         link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=7gvofti_Vl8hyDtZqVxuAwQSyYOaODOT&authKey=WBGICfV9oJXjeXL01KGcxg6FFzgyLgejgle0ZFRQpHMWDha4HftPvqfBGojiSHko&noverify=0&group_code=474128303",
       },
       {
-        text: "tts-vue问题反馈群②(702034846)-已满",
+        text: "tts-vue反馈群②(702034846)-已满",
         icon: "group",
         link: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=5-vK4VI8ueWoF-cBDl6PjBzfDeWRqmvw&authKey=ZHIxqKeYEGewEZFtKtT5o1UNbTtAEnMOe4vH2haBIxA8edc1U%2F4rn5R9RKSv5%2BRF&noverify=0&group_code=702034846",
       },
       {
-        text: "tts-vue问题反馈群①(752801820)-已满",
+        text: "tts-vue反馈群①(752801820)-已满",
         icon: "group",
         link: "https://jq.qq.com/?_wv=1027&k=bMNZfGxs",
       },
